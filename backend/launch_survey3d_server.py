0c9aebca6d6cce7feaa0c03e54ec645cfe72b94d
nielsen00 <tomnlink@hotmail.com>
2025-06-17 10:09:50 -0500

    Change launch command

--- backend/launch_survey3d_server.py
+++ backend/launch_survey3d_server.py
@@ -1,6 +1,5 @@
 import os
 import climber_core_utilities.load_config as load_config
 
-sys_config = load_config.system()
-cmd = "uvicorn main:app --host " + str(sys_config["ipExec"]) + " --port 5002  --log-level warning"
+cmd = "uvicorn main:app --host " + str(load_config.system()["ipExec"]) + " --port 5002  --log-level warning"
 os.system(cmd)
