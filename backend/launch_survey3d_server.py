48671b1e356a222a6a7f563c81ec62c3687a42b1
BB Rig <uchicago.bci@gmail.com>
2025-07-02 15:11:27 -0500

    Launch on 5003 port

--- backend/launch_survey3d_server.py
+++ backend/launch_survey3d_server.py
@@ -4,7 +4,7 @@ import climber_core_utilities.load_config as load_config
 
 ip = load_config.system()["ipExec"]
 
-rich.print(f"\tStarting survey3d server at http://{ip}:5002")
+rich.print(f"\tStarting survey3d server at http://{ip}:5003")
 
-cmd = f"uvicorn main:app --host {ip} --port 5002  --log-level warning"
+cmd = f"uvicorn main:app --host {ip} --port 5003 --log-level warning"
 os.system(cmd)
