1ba98e4c59fd6320a4a52a12c3264b9b0ca8c1a4
nielsen00 <tomnlink@hotmail.com>
2025-09-26 10:31:43 -0500

    Skip configs that are missing "survey3d"

--- backend/survey_manager_climber.py
+++ backend/survey_manager_climber.py
@@ -17,7 +17,7 @@ class SurveyManagerClimber(SurveyManager):
         self.config = {}
         climber_participant_config = ccu.load_config.participant()
         for p in climber_participant_config:
-            if "survey3d" in self.config[p]:
+            if "survey3d" in climber_participant_config[p]:
                 self.config[p] = climber_participant_config[p]["survey3d"]
         
         self.dataPath = ""
