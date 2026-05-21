5fb94f72ed24a3bd312a52ff7fcdabd80bd2e49a
nielsen00 <73450322+nielsen00@users.noreply.github.com>
2026-05-21 10:00:52 -0500

    Use host instead of system to get ip, use subprocess

--- backend/launch_survey3d_server.py
+++ backend/launch_survey3d_server.py
@@ -1,10 +1,10 @@
-import os
+import subprocess
 import rich
 import climber_core_utilities.load_config as load_config
 
-ip = load_config.system()["ipExec"]
+ip = load_config.host()["ip"]
 
 rich.print(f"\tStarting survey3d server at http://{ip}:5003")
 
 cmd = f"uvicorn main:app --host {ip} --port 5003 --log-level warning"
-os.system(cmd)
+subprocess.run(cmd)
